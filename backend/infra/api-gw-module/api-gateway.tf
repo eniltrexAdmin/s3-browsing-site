41e8552182bf9633e3c738b311dda2e7db54f511
Cesc <francesc.travesa@gmail.com>
2024-09-11 19:33:37 +0200

    I think thats it

--- backend/infra/api-gw-module/api-gateway.tf
+++ backend/infra/api-gw-module/api-gateway.tf
@@ -18,8 +18,8 @@ resource "aws_route53_record" "api_domain" {
   type    = "A"
 
   alias {
-    name                   = aws_api_gateway_domain_name.custom_domain.domain_name
-    zone_id                = aws_api_gateway_domain_name.custom_domain.regional_zone_id
+    name                   = aws_api_gateway_domain_name.custom_domain.cloudfront_domain_name
+    zone_id                = aws_api_gateway_domain_name.custom_domain.cloudfront_zone_id
     evaluate_target_health = false
   }
 }
