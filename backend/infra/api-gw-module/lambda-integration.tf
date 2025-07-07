64c92fe7e030f9c6653758ddbc0e3287144a7152
Cesc <francesc.travesa@gmail.com>
2025-07-07 17:42:09 +0200

    Correct domain name

--- backend/infra/api-gw-module/lambda-integration.tf
+++ backend/infra/api-gw-module/lambda-integration.tf
@@ -52,7 +52,7 @@ resource "aws_api_gateway_deployment" "api_deployment" {
 resource "aws_api_gateway_base_path_mapping" "path_mapping" {
   api_id      = aws_api_gateway_rest_api.api.id
   stage_name  = aws_api_gateway_deployment.api_deployment.stage_name
-  domain_name = var.domain_name
+  domain_name = aws_api_gateway_domain_name.custom_domain.domain_name
 }
 
 
