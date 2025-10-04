5b718131435fe5f847d76cc9b425b61d669fc092
Cesc <francesc.travesa@gmail.com>
2025-10-04 20:43:47 +0200

    adds the new lambda with the presign url code and the infra and the pipeline

---
+++ backend/presign-url/infra/production/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_version = ">= 1.9.0"
+  backend "s3" {
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-3"
+}
+
+
+variable "bucket_name" {
+  description = "The name of the bucket to browse"
+}
+
+# id -> data.terraform_remote_state.api_gateway.outputs.api_gateway_name
+module "presign-lambda" {
+  source = "../../../infra/lambda-module"
+  lambda_name = "${var.bucket_name}-presign-url"
+  lambda_file_full_path = "../../../../pre-sign-url"
+  executable_file = "pre-sign-program"
+  bucket_name = var.bucket_name
+  lambda_env_variables = {
+    "BUCKET" = var.bucket_name
+  }
+}
+
+
+output "lambda_function_name" {
+  value = module.presign-lambda.lambda_function_name
+}
+
+output "lambda_invoke_arn" {
+  value = module.presign-lambda.lambda_invoke_arn
+}
---
+++ backend/presign-url/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type PresignResponse struct {
+	URL string `json:"url"`
+}
+
+func handler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		log.Printf("Unable to load SDK config: %v", err)
+		return errorResponse(http.StatusInternalServerError, err.Error()), nil
+	}
+
+	bucket := event.QueryStringParameters["bucket"]
+	key := event.QueryStringParameters["key"]
+
+	if bucket == "" || key == "" {
+		return errorResponse(http.StatusBadRequest, "bucket and key query parameters are required"), nil
+	}
+
+	client := s3.NewFromConfig(cfg)
+	presigner := s3.NewPresignClient(client)
+
+	psReq, err := presigner.PresignGetObject(ctx, &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	}, func(opts *s3.PresignOptions) {
+		opts.Expires = time.Duration(lifetimeSecs * int64(time.Second))
+	})
+	if err != nil {
+		log.Printf("Couldn't get a presigned request to get %v:%v. Here's why: %v\n",
+			bucket, key, err)
+	}
+
+	respBody, err := json.Marshal(PresignResponse{URL: psReq.URL})
+	if err != nil {
+		log.Printf("Failed to marshal response: %v", err)
+		return errorResponse(http.StatusInternalServerError, err.Error()), nil
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       string(respBody),
+		Headers: map[string]string{
+			"Content-Type":                 "application/json",
+			"Access-Control-Allow-Origin":  "*",
+			"Access-Control-Allow-Methods": "GET,OPTIONS",
+			"Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key",
+		},
+	}, nil
+}
+
+func errorResponse(status int, msg string) events.APIGatewayProxyResponse {
+	return events.APIGatewayProxyResponse{
+		StatusCode: status,
+		Body:       fmt.Sprintf(`{"error":"%s"}`, msg),
+		Headers: map[string]string{
+			"Content-Type":                 "application/json",
+			"Access-Control-Allow-Origin":  "*",
+			"Access-Control-Allow-Methods": "GET,OPTIONS",
+			"Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key",
+		},
+	}
+}
+
+func main() {
+	lambda.Start(handler)
+}
